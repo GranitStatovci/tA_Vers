00015b4528223dd91dc9688d0fde412628c20df9
Graniti 05 <nitist2@hotmail.com>
2022-03-28 14:07:16 +0100

    First Commit

---
+++ tA_Vers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tA_Vers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
